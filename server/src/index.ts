155409c158de186f5dce55cf0347b4681c58f0e8
John Farrell <johnfarrell@fastmail.com>
2025-09-24 22:00:19 +0100

    basic api for interacting with rooms

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import cookie from "@fastify/cookie";
+import { createRoom, getRoom } from "./store";
+import { z } from "zod";
 
 async function start() {
   const app = Fastify({ logger: true });
@@ -9,6 +11,22 @@ async function start() {
 
   app.get("/health", async () => {
     return { status: "ok" };
+  });
+
+  // create a new room
+  app.post("/rooms", async (_req, reply) => {
+    const room = createRoom();
+    reply.send({ id: room.id });
+  });
+
+  // get room state
+  app.get("/rooms/:id", async (req, reply) => {
+    const { id } = z
+      .object({ id: z.string().min(3).max(12) })
+      .parse(req.params);
+    const room = getRoom(id);
+    if (!room) return reply.code(404).send({ error: "not_found" });
+    reply.send(room);
   });
 
   const PORT = Number(process.env.PORT || 4000);
