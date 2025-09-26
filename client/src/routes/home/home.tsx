a585278ab62fcc3ec63263908fd8f580a9c15b5f
John Farrell <johnfarrell@fastmail.com>
2025-09-26 10:51:46 +0100

    minor corrections to lobby screen

--- client/src/routes/home/home.tsx
+++ client/src/routes/home/home.tsx
@@ -12,7 +12,6 @@ export function Home() {
   async function handleCreate() {
     // make a request to the server to create a lobby for our user
     // take the response and navigate to the lobby page using the lobby id
-
     const res = await createNewLobby({
       playerId: player.id,
       name: player.name,
@@ -27,13 +26,15 @@ export function Home() {
 
       <div className={styles.card}>
         <h1 className={styles.title}>♠ Big Two ♦</h1>
-        <p className={styles.subtitle}>Set your name</p>
-        <input
-          type="text"
-          onChange={updateUsername}
-          value={player.name}
-          className={styles.usernameInput}
-        />
+        <div>
+          <p className={styles.subtitle}>Set your name</p>
+          <input
+            type="text"
+            onChange={updateUsername}
+            value={player.name}
+            className={styles.usernameInput}
+          />
+        </div>
         <div className={styles.actions}>
           <button className={styles.createBtn} onClick={handleCreate}>
             Create a Lobby
